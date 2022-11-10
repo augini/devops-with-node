af87b4ff52ad95ce0691fe4d53123af08e23b477
Farrukh Atabekov <augini@inbox.ru>
2022-11-10 20:18:03 +0500

    feat: initiall commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+const port = process.env.port || 3000;
+
+app.get("/", (req, res) => {
+  res.send("api is up and running and updated");
+});
+
+app.listen(port, () => console.log("Listening on port: ", port));
