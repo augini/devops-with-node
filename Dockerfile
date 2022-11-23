28e94c74be7c6bdb0e1e5fbc38c24acbafa0fad0
Farrukh Atabekov <augini@inbox.ru>
2022-11-24 02:25:01 +0500

    feat: push to docker hub

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:18-alpine
+FROM --platform=linux/amd64 node:18-alpine
 ENV NODE_ENV=production
 WORKDIR /app
 COPY ["package.json", "package-lock.json", "./"]
--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const port = process.env.PORT || 3000;
 
 app.get("/api/v1", (req, res) => {
   console.log("req received");
-  res.send("api is up and been running and again");
+  res.send("api is up and been running. Just pushed to docker hub");
 });
 
 app.enable("trust proxy");
