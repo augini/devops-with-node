a786e57e6e2965114ae72e258750ceef47a595f6
Farrukh Atabekov <augini@inbox.ru>
2022-11-29 00:16:07 +0500

    fix: update restart shell script

--- restart_prod.sh
+++ restart_prod.sh
@@ -1,1 +1,2 @@
-docker-compose down && docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build
+# docker-compose down && docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build
+docker stack deploy -c docker-compose.yml -c docker-compose.prod.yml up docker-node
