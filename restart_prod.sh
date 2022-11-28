a59cd6d4c8c703104e39165547c641c551cc258a
Farrukh Atabekov <augini@inbox.ru>
2022-11-29 00:17:43 +0500

    fix: remove rollback config, update restart bash script

--- restart_prod.sh
+++ restart_prod.sh
@@ -1,2 +1,2 @@
 # docker-compose down && docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build
-docker stack deploy -c docker-compose.yml -c docker-compose.prod.yml up docker-node
+docker stack deploy -c docker-compose.yml -c docker-compose.prod.yml docker-node
